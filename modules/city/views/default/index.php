903c8a060d44302ef69303b7a897fb67654f1f78
Eugene Dyakonov <eugene.dyakonov@gmail.com>
2021-05-26 06:06:25 +0400

    Update city index template

--- modules/city/views/default/index.php
+++ modules/city/views/default/index.php
@@ -1,12 +1,37 @@
-<div class="city-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model app\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'Cities';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-index">
+    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="body-content">
+        <?php if (count($cities) > 0) :?>
+            <div class="row">
+                <?php foreach ($cities as $city) : ?>
+                    <div class="col-lg-4">
+                        <h2><?= $city->name ?></h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                            fugiat nulla pariatur.</p>
+                    </div>
+                <? endforeach; ?>
+            </div>
+        <?php else: ?>
+        <div class="row">
+            <div class="col-lg-4">
+                <h4>Список городов пуст.</h4>
+            </div>
+        </div>
+
+        <?php endif; ?>
+    </div>
 </div>
