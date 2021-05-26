5e6ac0732d1b9cfb6163bd5578a85c3894b3f88a
Eugene Dyakonov <eugene.dyakonov@gmail.com>
2021-05-26 06:07:01 +0400

    Add view template for showing information about city

---
+++ modules/city/views/default/view.php
@@ -0,0 +1,37 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model app\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'City ID: ' . $city->id ?? 'null';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-index">
+    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="body-content">
+        <div class="row">
+            <?php if (count($cities) > 0) :?>
+            <div class="col-lg-4">
+                <h2>City 1</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+            </div>
+            <div class="col-lg-4">
+                <h2>City 1</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+            </div>
+            <? else: ?>
+
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
