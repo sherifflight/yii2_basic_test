f815247eb91f0d15354aef4dbbf904d3531f9d28
Eugene Dyakonov <eugene.dyakonov@gmail.com>
2021-05-28 02:16:36 +0400

    Update page with list of feedbacks of a city

--- modules/city/views/default/view.php
+++ modules/city/views/default/view.php
@@ -4,33 +4,24 @@
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model app\models\LoginForm */
 
-use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
-
 $this->title = 'City ID: ' . $city->id ?? 'null';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-index">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1>Список отзывов города: <?= $city->name ?>.</h1>
     <div class="body-content">
         <div class="row">
-            <?php if (count($cities) > 0) :?>
-            <div class="col-lg-4">
-                <h2>City 1</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-            </div>
-            <div class="col-lg-4">
-                <h2>City 1</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-            </div>
+            <?php if (isset($city->feedbacks) && (count($city->feedbacks) > 0)) :?>
+                <?php foreach ($city->feedbacks as $feedback) : ?>
+                    <div class="col-lg-4">
+                        <h2><?= $feedback->title ?></h2>
+                        <p><?= $feedback->text ?></p>
+                    </div>
+                <?php endforeach; ?>
             <? else: ?>
-
+                <div class="col-lg-4">
+                    <h4>У города нет отзывов.</h4>
+                </div>
             <?php endif; ?>
         </div>
     </div>
